abd6dcfc26aa5a794b2510ca7673aecf77906b9f
maanoel <manoelvitorbrito@gmail.com>
2023-01-24 08:23:14 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tj8q9x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tj8q9x)
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.css';
+
+const Button = ({ children, ...rest }) => (
+  <button onClick={() => console.log('ButtonClick')} {...rest}>
+    {children}
+  </button>
+);
+
+const withClick = (Component) => {
+  const handleClick = () => {
+    console.log('WithClick');
+  };
+
+  return (props) => {
+    return <Component {...props} onClick={handleClick} />;
+  };
+};
+
+const MyButton = withClick(Button);
+
+export default function App() {
+  return <MyButton onClick={() => console.log('AppClick')}>Submit</MyButton>;
+}
